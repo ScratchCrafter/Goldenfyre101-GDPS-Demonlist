be94e3f5bfbeb1161e8dbe8803d682a83aab7b3c
ScratchCrafter <139852200+ScratchCrafter@users.noreply.github.com>
2026-07-01 00:48:57 +1000

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -71,7 +71,7 @@ export default {
                                 <img v-if="record.mobile" :src="\`/assets/phone-landscape\${store.dark ? '-dark' : ''}.svg\`" alt="Mobile">
                             </td>
                             <td class="hz">
-                                <p>{{ record.hz }}Enj</p>
+                                <p></p>
                             </td>
                         </tr>
                     </table>
