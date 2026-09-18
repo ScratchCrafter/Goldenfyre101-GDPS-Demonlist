7ddc3ebdb355c7184735fc295790f114e0ada232
ScratchCrafter <139852200+ScratchCrafter@users.noreply.github.com>
2026-09-18 15:52:19 +1000

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -51,7 +51,7 @@ export default {
                     <ul class="stats">
                         <li>
                             <div class="type-title-sm">Points when completed</div>
-                            <p>{{ score(selected + 1, 100, level.percentToQualify) }}</p>
+                            <p>{{ selected + 1 <= 50 ? score(selected + 1, 100, level.percentToQualify) : 0 }}</p>
                         </li>
                         <li>
                             <div class="type-title-sm">ID</div>
