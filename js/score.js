03134021f285122776a44dd0a4d8b3d6387e9f76
hexomexo79-pixel <hexomexo79@gmail.com>
2026-09-22 08:06:10 -0500

    Update score.js

--- js/score.js
+++ js/score.js
@@ -11,7 +11,7 @@ const scale = 3;
  * @returns {Number}
  */
 export function score(rank, percent, minPercent) {
-    if (rank > 50) {
+    if (rank > 75) {
         return 0;
     }
 
@@ -21,7 +21,7 @@ export function score(rank, percent, minPercent) {
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
     */
     // New formula
-    let score = (-66.6667*Math.pow(rank-1, 0.4) + 350) *
+    let score = (-57.5*Math.pow(rank-1, 0.4) + 300) *
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
 
     score = Math.max(0, score);
